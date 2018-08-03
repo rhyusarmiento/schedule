218d76e93e3c6f36499da66515a4a39ab86638c7
Rhyu Sarmiento <rhyu.sar.15@gmail.com>
2018-08-03 21:03:29 +0000

    library data component finshed

--- src/components/library/libraryCourse.js
+++ src/components/library/libraryCourse.js
@@ -1,20 +1,23 @@
 import React, { Component } from 'react';
+
 import Icon from '../icon';
 import Arrow from '../arrow';
+import Action from '../action';
 
 
 class LibraryCourse extends Component {
     render() {
         return (
             <div className="library-course">
-                <div className="library-course__title-icon">
+                <div className="library-course__title-check">
                     <label className="library-course__title">Problem Solveing</label>
                     { Icon('fas fa-check', 'library-course__icon')}
                 </div>
                 <Arrow className="library-course__arrow"/>
+                <Action className="library-course__action"/>
                 <div className="library-course__description">
                     <label>Course Description</label>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sed imperdiet nunc. Pellentesque porttitor tortor eget suscipit ullamcorper. Maecenas ut pulvinar risus. Mauris mattis nisl tortor, sed hendrerit augue tristique non. Praesent non magna lacus. Donec quam nisl, lobortis ut mi quis, ullamcorper sodales eros. Suspendisse dignissim egestas dolor ac scelerisque. Sed enim turpis, interdum eget elementum sit amet, vestibulum quis sem. Maecenas vitae risus.</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sed imperdiet nunc. Pellentesque porttitor tortor eget suscipit ullamcorper. Maecenas ut pulvinar risus. Mauris mattis nisl tortor, sed hendrerit augue tristique non.</p>
                 </div>
             </div>
         )
