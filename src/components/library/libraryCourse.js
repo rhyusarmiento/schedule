525be2bf19674d25ab125b39e02c374bcf936e58
Rhyu Sarmiento <rhyu.sar.15@gmail.com>
2018-08-06 23:25:30 +0000

    re-styled grid for library course

--- src/components/library/libraryCourse.js
+++ src/components/library/libraryCourse.js
@@ -7,43 +7,37 @@ import Icon from '../icon';
 import Arrow from '../arrow';
 import Action from '../action';
 
+import AnimateHeight from 'react-animate-height';
 
 class LibraryCourse extends Component {
     
     constructor(props) {
-        super(props)
+        super(props);
         
         this.state = {
-            status: true
-        }
+            status: true,
+            height: 0
+        };
     }
     
-    renderDescription = function () {
-        if(!this.state.status) {
-            document.getElementById('library-course').classList.add('library-course-selected')
-            return (
-            <div className="library-course__description">
-                <label>Course Description</label>
-                <p>
-                    { this.props.description }
-                </p>
-            </div>
-            )
+    handleCallback = function(status) {
+        let height = this.state.height == 0 ? 80 : 0;
+        if(!status) {
+            document.getElementById(this.id).classList.add('library-course-selected');
+        } else {
+            document.getElementById(this.id).classList.add('library-course-selected');
         }
+        this.setState({ 
+            status, 
+            height 
+        });
+        
     }.bind(this);
     
-    handleCallback = function(status) {
-        if(!status) {
-            document.getElementById('library-course').classList.add('library-course-selected')
-        } else {
-            document.getElementById('library-course').classList.add('library-course-selected')
-        }
-        this.setState({ status })
-    }.bind(this)
-    
     render() {
+        this.id = `library-course-${this.props.id}`
         return (
-            <div id="library-course" className="library-course">
+            <div id={this.id} className="library-course">
                 <div className="library-course__title-check">
                     <label className="library-course__title">{ this.props.title }</label>
                     { Icon('fas fa-check', 'library-course__icon')}
@@ -54,11 +48,23 @@ class LibraryCourse extends Component {
                     id={this.props.id} 
                     className="library-course__arrow"
                 />
-                <Action 
+                <Action
+                    id={this.props.id}
                     onClick={() => this.props.toggleEnrolled(this.props.id)} 
                     className="library-course__action"
                 />
-                { this.renderDescription() }
+                { this.renderDescription }
+                <AnimateHeight 
+                    duration={300}
+                    height={this.state.height}
+                >
+                    <div className="library-course__description">
+                        <label>Course Description</label>
+                        <p>
+                            { this.props.description }
+                        </p>
+                    </div>
+                </AnimateHeight>
             </div>
         )
     }
